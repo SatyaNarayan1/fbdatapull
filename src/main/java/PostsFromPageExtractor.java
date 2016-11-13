ce40edc63f8643be0ed224e61844a4b9132cfc2c
Satya Narayan <satnara1@in.ibm.com>
2016-11-13 13:00:24 +0530

    changes for pagination

--- src/main/java/PostsFromPageExtractor.java
+++ src/main/java/PostsFromPageExtractor.java
@@ -9,8 +9,6 @@ import org.json.simple.JSONObject;
 public class PostsFromPageExtractor {
 
     /**
-     * A simple Facebook4J client which
-     * illustrates how to access group feeds / posts / comments.
      *
      * @param args
      * @throws FacebookException
@@ -19,25 +17,10 @@ public class PostsFromPageExtractor {
 
         // Generate facebook instance.
         Facebook facebook = new FacebookFactory().getInstance();
-      //  System.out.println(facebook.getConfiguration().isJSONStoreEnabled() );
-
-        // Use default values for oauth app id.
-//        facebook.setOAuthAppId("296251414082104","cb99043a762d7fbb1f16c6f19284c9fa");
-//        // Get an access token from:
-//        // https://developers.facebook.com/tools/explorer
-//        // Copy and paste it below.
-//        String accessTokenString = "EAACEdEose0cBAFmYg6yPbngHfhQw8XdNXS3qMDiDQPPvvVXnQdpARCm3ZAbnF09noOWcVCQ0PfhYt39Qz8PGZBRGoA5DaTOaId99msz5z8t5D0b5Vvpu1moZAouOnsBA4co7drOyhzotLLdiJMizZCfGlOinFxZCFRZBWGJmDq2AZDZD";
-//        AccessToken at = new AccessToken(accessTokenString);
-//        // Set access token.
-//        facebook.setOAuthAccessToken(at);
-
-        // We're done.
-        // Access group feeds.
-        // You can get the group ID from:
-        // https://developers.facebook.com/tools/explorer
 
         // Set limit to 25 feeds.
         String feedId="";
+
         if(args.length<1)
         {
             feedId="192320847482725";
@@ -46,81 +29,96 @@ public class PostsFromPageExtractor {
         {
             feedId=args[0];
         }
-        ResponseList<Post> feeds = facebook.getFeed(feedId);
-//ResponseList<Post> feeds = facebook.getFeed("700329662",
-        //          new Reading().limit(25));
+
+        int currentCount = 0;
         try {
-        PrintWriter postWriter = new PrintWriter(feedId+"_post.json", "UTF-8");
-        PrintWriter commentWriter = new PrintWriter(feedId+"_comment.json", "UTF-8");
-            PrintWriter likesWriter = new PrintWriter(feedId+"_likes.json", "UTF-8");
+            PrintWriter postWriter = new PrintWriter(feedId + "_post.json", "UTF-8");
+            PrintWriter commentWriter = new PrintWriter(feedId + "_comment.json", "UTF-8");
+            PrintWriter likesWriter = new PrintWriter(feedId + "_likes.json", "UTF-8");
 
-        // For all xx feeds...
-        for (int i = 0; i < feeds.size(); i++) {
-            // Get post.
-            Post post = feeds.get(i);
-            // Get (string) message.
-            String message = post.getMessage();
-            // Print out the message.
 
-            PagableList<Comment> comments = post.getComments();
-            PagableList<Like> likes = post.getLikes();
-          //  String date = post.getCreatedTime().toString();
-           // String name = post.getFrom().getName();
-            String id = post.getId();
-            String json  = DataObjectFactory.getRawJSON(post);
-           // System.out.println(json);
-            postWriter.println(json);
-            System.out.println(comments);
-            System.out.println(likes);
-            String parentId = id;
-            for (int j = 0; j < comments.size(); j++) {
+        while(true){
+
+            ResponseList<Post> feeds = facebook.getFeed(feedId);
+
+            int size=feeds.size();
+
+            //System.out.println("Fetching next "+size+"posts ");
+            // For all xx feeds...
+            for (int i = 0; i < size; i++) {
                 // Get post.
-                Comment comment = comments.get(j);
-
-                String msg = comment.getMessage();
-                String commentId = comment.getId();
-                String created_time = comment.getCreatedTime().toString();
-                Integer likeCount = comment.getLikeCount();
-                String from  = comment.getFrom().getName();
-                String fromId = comment.getFrom().getId();
-                JSONObject obj = new JSONObject();
-                obj.put("parentId", parentId);
-                obj.put("id", commentId);
-                obj.put("message",msg);
-                obj.put("created_time",created_time);
-                obj.put("likeCount",likeCount);
-                obj.put("from",from);
-                obj.put("fromId",fromId);
-
-
-                // Print out the message.
-               // String commentJson  = DataObjectFactory.getRawJSON(comment);
-
-                commentWriter.println(obj.toString());
-
-            }
-            for (int j = 0; j < likes.size(); j++) {
-                // Get post.
-                Like like = likes.get(j);
+                Post post = feeds.get(i);
                 // Get (string) message.
-                String likeId = like.getId();
-                String category = like.getCategory();
-                String name = like.getName();
-              //  String lik eJson  = DataObjectFactory.getRawJSON(like);
+                String message = post.getMessage();
 
-                JSONObject obj = new JSONObject();
-                obj.put("parentId", parentId);
-                obj.put("id", likeId);
-                obj.put("name",name);
-                obj.put("category",category);
-                likesWriter.println(obj.toString());
+
+                PagableList<Comment> comments = post.getComments();
+
+
+                PagableList<Like> likes = post.getLikes();
+                String id = post.getId();
+                String json = DataObjectFactory.getRawJSON(post);
+                System.out.println(json);
+                postWriter.println(json);
+                //System.out.println(comments);
+                //System.out.println(likes);
+                String parentId = id;
+                for (int j = 0; j < comments.size(); j++) {
+                    // Get post.
+                    Comment comment = comments.get(j);
+
+                    String msg = comment.getMessage();
+                    String commentId = comment.getId();
+                    String created_time = comment.getCreatedTime().toString();
+                    Integer likeCount = comment.getLikeCount();
+                    String from = comment.getFrom().getName();
+                    String fromId = comment.getFrom().getId();
+                    JSONObject obj = new JSONObject();
+                    obj.put("parentId", parentId);
+                    obj.put("id", commentId);
+                    obj.put("message", msg);
+                    obj.put("created_time", created_time);
+                    obj.put("likeCount", likeCount);
+                    obj.put("from", from);
+                    obj.put("fromId", fromId);
+
+
+                    // Print out the message.
+                    // String commentJson  = DataObjectFactory.getRawJSON(comment);
+
+                    commentWriter.println(obj.toString());
+
+                }
+                for (int j = 0; j < likes.size(); j++) {
+                    // Get post.
+                    Like like = likes.get(j);
+                    // Get (string) message.
+                    String likeId = like.getId();
+                    String category = like.getCategory();
+                    String name = like.getName();
+                    //  String lik eJson  = DataObjectFactory.getRawJSON(like);
+
+                    JSONObject obj = new JSONObject();
+                    obj.put("parentId", parentId);
+                    obj.put("id", likeId);
+                    obj.put("name", name);
+                    obj.put("category", category);
+                    likesWriter.println(obj.toString());
+
+                }
+
 
             }
 
-
+            postWriter.flush();
+            commentWriter.flush();
+            likesWriter.flush();
+            Paging<Post> page = feeds.getPaging();
+            feeds=facebook.fetchNext(page);
+            if(feeds.size()<1)break;
         }
             postWriter.close();
-             commentWriter.close();
+            commentWriter.close();
             likesWriter.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
